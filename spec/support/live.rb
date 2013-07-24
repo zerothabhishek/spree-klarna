403321a4d4dde742f022a7784b2ff1ff306a76f0
Tobias Bohwalli <hi@futhr.io>
2013-07-24 12:15:10 +0200

    Fix path for live.yml

--- spec/support/live.rb
+++ spec/support/live.rb
@@ -1,5 +1,5 @@
 require 'yaml'
 
-raw_config = File.read('spec/support/live.yml')
+raw_config = File.read('live.yml')
 KLARNA_EDI = YAML.load(raw_config)['edi'] unless ENV['EDI']
 KLARNA_SS  = YAML.load(raw_config)['shared_secret'] unless ENV['KLARNA_SS']
