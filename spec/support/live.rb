85a6f386c4e4fbd2af03dd873e113e4e3b8f3231
Tobias Bohwalli <hi@futhr.io>
2013-07-26 12:37:14 +0200

    Fix test support yml reader that broke specs if no file exists.

--- spec/support/live.rb
+++ spec/support/live.rb
@@ -1,5 +1,7 @@
 require 'yaml'
 
-raw_config = File.read('live.yml')
-KLARNA_EDI = YAML.load(raw_config)['edi'] unless ENV['EDI']
-KLARNA_SS  = YAML.load(raw_config)['shared_secret'] unless ENV['KLARNA_SS']
+if File.exist?('live.yml')
+  raw_config = File.read('live.yml')
+  KLARNA_EDI = YAML.load(raw_config)['edi'] unless ENV['EDI']
+  KLARNA_SS  = YAML.load(raw_config)['shared_secret'] unless ENV['KLARNA_SS']
+end
