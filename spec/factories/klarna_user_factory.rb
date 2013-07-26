2c54e94d1b827213896d4822b79138f6234dda01
Tobias Bohwalli <hi@futhr.io>
2013-07-26 12:39:02 +0200

    Rename factory.

--- spec/factories/klarna_user_factory.rb
+++ spec/factories/klarna_user_factory.rb
@@ -1,15 +1,11 @@
 FactoryGirl.define do
-  factory :test_person, parent: :user do
+  factory :klarna_user, aliases: [:test_person], parent: :user do
     trait :swedish do
       email "checkout-se@testdrive.klarna.com"
-      zip 12345
-      personal_identity_number "410321-9202"
     end
 
     trait :finnish do
       email "checkout-fi@testdrive.klarna.com"
-      zip 28100
-      personal_identity_number "190122-829F"
     end
   end
 end
